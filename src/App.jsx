e1e24f800f5481524d48bc7ea37377eabb2e2068
Carolina Barbieri <carolinabarbieri35@gmail.com>
2026-09-18 18:50:40 +0200

    create components

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
-import { useState } from 'react'
-import heroImg from './assets/hero.png'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <h1>Let's start React </h1>
+    <div>
+      <header>My first react project</header>
+
+      <main>
+        <section>here we goooo</section>
+      </main>
+
+      <footer>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+      </footer>
+    </div>
   )
 }
 
